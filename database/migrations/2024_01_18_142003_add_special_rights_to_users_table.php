97a16d87f0363b29e91e8305eb0aae1cd9b2996d
S-witsel <1059566@hr.nl>
2024-01-18 15:32:35 +0100

    Initial commit

---
+++ database/migrations/2024_01_18_142003_add_special_rights_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('special_rights')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('special_rights');
+        });
+    }
+};
